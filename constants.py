d08f9847e84c5c2e8d9caa20422b9c5341fac4c3
Melissendra <melissendra77@gmail.com>
2018-11-03 16:15:48 +0100

    creation virtual environement, maze_draw and constants files

---
+++ constants.py
@@ -0,0 +1,20 @@
+"""Games' constants"""
+
+# Window properties
+sprite_size = 32
+sprite_nb = 15
+window_size = sprite_size * sprite_nb
+
+# window's appearance
+window_title = "Help MacGyver to escape"
+window_icone = "venv/ressources/MacGyver.png"
+
+# differents images
+needle_img = "venv/ressources/aiguille.png"
+ether_img = "venv/ressources/ether.png"
+plastic_tube_img = "venv/ressources/tube_plastique.png"
+syringhe_img = "venv/ressources/seringue.png"
+macGyver_img = "venv/ressources/MacGyver.png"
+guardian_img = "venv/ressources/Gardien.png"
+floor_img = "venv/ressources/floor.png"
+
