8c3ea6d0ac28e95d87d30ed441c83e85ac1b3f46
Melissendra <melissendra77@gmail.com>
2018-11-20 15:35:45 +0100

    Modification of the maze class and rectifying  some spelling

--- Maze.py
+++ Maze.py
@@ -1,19 +1,19 @@
 import pygame
 
 from pygame.locals import *
-from sys import exit
+
 from constants import *
 from MacGyver import *
 
 
 """Creation of the maze"""
 class Maze():
-    # Window initialization
-    def __init__(self, window):
+
+    def __init__(self, window='default'):
         self.window = window
         pygame.init()
 
-        # Opening window's Maze Game
+    def home_page(self):
         window = pygame.display.set_mode((window_size, window_size))
 
         # Window's icon
@@ -23,53 +23,34 @@ class Maze():
         # Title
         pygame.display.set_caption(window_title)
 
+        # opening of the home page
+        pygame.time.Clock().tick(30)
+        home = pygame.image.load(home_page).convert()
+        window.blit(home, (0, 0))
+
+        # Welcome sentence
+        font = pygame.font.Font(None, 35)
+        welcome_text = font.render ("Welcome to MacGyver's Maze !", 0, (58, 252, 139))
+        window.blit(welcome_text, (30, 70))
+
+        pygame.display.flip()
 
     # principal loop
-    def game():
-        gaming =True
-        while gaming:
-            # Opening of the Home's page
-            home = pygame.image.load(home_page).convert()
-            window.blit(home, (0, 0))
-
-            # refresh
-            pygame.display.flip()
+    def run(self):
+        # Window initialization
+        self.home_page()
+        running = True
+        while running:
 
             for event in pygame.event.get():
-                if event.type == QUIT:
-                    gaming = False
-                    exit()
-
-
-#     carry_on_game = True
-#     carry_on_home =True
-
-#     # Welcome loop
-#     while carry_on_home:
-#         pygame.time.Clock().tick(30)
-
-#         sys_font = pygame.font.SysFont("Arial", 24)
-#         welcome_text = sys_font.render("Welcome to MacGyver's Maze Game !", 0, (58,252,139))
-#         window.blit(welcome_text, (15,70))
-
-#         for event in pygame.event.get():
-#             # For leaving the game
-#             if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
-#                pygame.quit()
-#                sys.exit()
-#                # Play variable
-#                play = 0
-
-            
-#             elif event.type == KEYDOWN:
-#                 if event.key == K_RETURN:
-#                     carry_on_home = False
-#                     play = "maze_draw1.txt"
+                if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+                    running = False
 
         
-#         pygame.display.flip()
-                
-               
+
+if __name__ == '__main__':
+    Maze().run()
+
 
 
 
--- constants.py
+++ constants.py
@@ -7,15 +7,15 @@ window_size = sprite_size * sprite_nb
 
 # window's appearance
 window_title = "Help MacGyver to escape"
-window_icone = "ressources/macGyver.png"
+window_icone = "resources/macGyver.png"
 
 # differents images
-home_page = "ressources/homeMG.jpg"
-needle_img = "ressources/needle.png"
-ether_img = "ressources/ether.png"
-plastic_tube_img = "ressources/plastic_tube.png"
-syringhe_img = "ressources/syringhe.png"
-macGyver_img = "ressources/macGyver.png"
-guardian_img = "ressources/Gardian.png"
-floor_img = "ressources/floor.png"
-death_img = "ressources/rip.png"
+home_page = "resources/homeMG.jpg"
+needle_img = "resources/needle.png"
+ether_img = "resources/ether.png"
+plastic_tube_img = "resources/plastic_tube.png"
+syringhe_img = "resources/syringhe.png"
+macGyver_img = "resources/macGyver.png"
+guardian_img = "resources/Gardian.png"
+floor_img = "resources/floor.png"
+death_img = "resources/rip.png"
