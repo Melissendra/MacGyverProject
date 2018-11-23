26c4db0f75b13f29c9681bd73f6671956a0e1201
Melissendra <35914839+melissendra@users.noreply.github.com>
2018-11-23 11:16:00 +0100

    new background image and beginning of button creation

--- constants.py
+++ constants.py
@@ -7,15 +7,19 @@ window_size = sprite_size * sprite_nb
 
 # window's appearance
 window_title = "Help MacGyver to escape"
-window_icone = "resources/macGyver.png"
+window_icon = "resources/macGyver.png"
 
 # differents images
-home_page = "resources/homeMG.jpg"
+home_page = "resources/explosion.png"
 needle_img = "resources/needle.png"
 ether_img = "resources/ether.png"
 plastic_tube_img = "resources/plastic_tube.png"
-syringhe_img = "resources/syringhe.png"
+syringe_img = "resources/syringe.png"
 macGyver_img = "resources/macGyver.png"
-guardian_img = "resources/Gardian.png"
+guardian_img = "resources/gardian.png"
 floor_img = "resources/floor.png"
 death_img = "resources/rip.png"
+
+# colors used in the code
+before_clicked = (58, 252, 139)
+after_clicked = (41, 163, 102)
